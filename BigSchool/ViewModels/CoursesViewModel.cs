807a5d65a322fbe68cd881aa9e8d6e64f12aa006
VanThien <thienvan2464@gmail.com>
2021-07-28 13:07:08 +0700

    Add project files.

---
+++ BigSchool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CoursesViewModel
+    {
+        public IEnumerable<Course> UpcommingCourses { get; set; }
+        public IEnumerable<Following> Followings { get; set; }
+        public IEnumerable<Attendance> Attendances { get; set; }
+        public bool ShowAction { get; set; }
+    }
+}
